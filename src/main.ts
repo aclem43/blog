c292c8f9ac46c3385122a06790bd5479f570a375
aclem43 <aclemeur@gmail.com>
2023-05-10 10:54:57 +1000

    Initial Setup

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({})
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
